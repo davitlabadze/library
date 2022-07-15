359f3950c750043d4a8dcf754930894360501e24
davitlabadze <labadzedato18@gmail.com>
2022-07-15 14:53:39 +0400

    get recent action

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Author;
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    public function dashboard()
+    {
+        $authorCount = Author::count();
+        $bookCount = Book::count();
+        $books = Book::with('authors')->orderBy('id', 'desc')->get();
+        return view('adminPanel.dashboard', ['books' => $books, 'bookCount' => $bookCount, 'authorCount' => $authorCount]);
+    }
 }

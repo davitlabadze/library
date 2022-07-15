63901d335520866984993d6437c20ad3a655a666
davitlabadze <labadzedato18@gmail.com>
2022-07-15 14:19:21 +0400

    create model

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Author extends Model
 {
     use HasFactory;
+    public $guarded = [];
+    public function books()
+    {
+        return $this->belongsToMany(Book::class);
+    }
 }

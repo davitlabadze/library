f1d6fa16d23ee01beaea7e7953dd8904def2483a
davitlabadze <labadzedato18@gmail.com>
2022-09-09 23:03:05 +0400

    style: button

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,32 +79,32 @@
         <div class="grid grid-cols-3 mt-12 mb-12 text-center ">
             <div class="grid grid-cols-2 mb-12 ">
                 <img src="{{ asset('image/4.jpeg') }}" class="h-auto ml-28 w-36 " alt="image">
-                <div>
+                <div class="relative">
                     <h1>A Game of THRONES</h1>
                     <p class="mt-2 text-gray-400">GEORGE R.R MARTIN</p>
                     <p class="w-16 mt-2 ml-24 text-white bg-green-500 rounded-md">Free</p>
                     <button
-                        class="p-2 mt-12 text-xl text-orange-400 border border-orange-400 rounded-3xl">Subscribe</button>
+                        class="absolute p-2 text-xl text-orange-400 border border-orange-400 left-20 bottom-2 rounded-3xl">Subscribe</button>
                 </div>
             </div>
             <div class="grid grid-cols-2 mb-12 ">
                 <img src="{{ asset('image/1.jpeg') }}" class="h-auto ml-28 w-36 " alt="image">
-                <div>
+                <div class="relative">
                     <h1>Harry Potter: and the Chamber of secrets</h1>
                     <p class="mt-2 text-gray-400">J.K ROWLING</p>
                     <p class="w-16 mt-2 ml-24 text-white bg-red-500 rounded-md">Busy</p>
                     <button
-                        class="p-2 mt-12 text-xl text-orange-400 border border-orange-400 rounded-3xl">Bookmark</button>
+                        class="absolute p-2 text-xl text-orange-400 border border-orange-400 left-20 bottom-2 rounded-3xl">Bookmark</button>
                 </div>
             </div>
-            <div class="grid grid-cols-2 mb-12 ">
+            <div class="grid grid-cols-2 mb-12">
                 <img src="{{ asset('image/2.jpeg') }}" class="h-auto ml-28 w-36 " alt="image">
-                <div>
+                <div class="relative">
                     <h1>The OPPOSITE of Fate hardcover</h1>
                     <p class="mt-2 text-gray-400">AMY TAN</p>
                     <p class="w-16 mt-2 ml-24 text-white bg-green-500 rounded-md">Free</p>
                     <button
-                        class="p-2 mt-12 text-xl text-orange-400 border border-orange-400 rounded-3xl">Subscribe</button>
+                        class="absolute p-2 text-xl text-orange-400 border border-orange-400 left-20 bottom-2 rounded-3xl">Subscribe</button>
                 </div>
             </div>
         </div>

8f39075ba729b582f5da62a0e5e511059df7c275
davitlabadze <labadzedato18@gmail.com>
2022-09-10 18:33:17 +0400

    style(books): add search input

--- resources/views/books.blade.php
+++ resources/views/books.blade.php
@@ -11,45 +11,48 @@
 </head>
 
 <body>
-    <div class="container gap-8 p-4 mx-auto space-y-3 columns-6">
+    <div class="container p-4 mx-auto text-center ">
+        <input type="search" placeholder="Title, author, or topics" name="search" id="search"
+            class="w-1/2 p-4 mb-12 text-orange-400 border border-orange-400 outline-none placeholder:text-orange-300 rounded-3xl">
+        <div class="gap-4 space-y-3 columns-6">
+            <img class="w-full shadow-lg cursor-pointer rounded-2xl " src="{{ asset('image/1.jpeg') }}" />
+            <img class="w-full shadow-lg cursor-pointer rounded-2xl " src="{{ asset('image/2.jpeg') }}" />
+            <img class="w-full shadow-lg cursor-pointer rounded-2xl " src="{{ asset('image/3.jpg') }}" />
+            <img class="w-full shadow-lg cursor-pointer rounded-2xl " src="{{ asset('image/4.jpeg') }}" />
+            <img class="w-full shadow-lg cursor-pointer rounded-2xl " src="{{ asset('image/5.jpeg') }}" />
 
-        <img class="w-full " src="{{ asset('image/1.jpeg') }}" />
-        <img class="w-full " src="{{ asset('image/2.jpeg') }}" />
-        <img class="w-full " src="{{ asset('image/3.jpg') }}" />
-        <img class="w-full " src="{{ asset('image/4.jpeg') }}" />
-        <img class="w-full " src="{{ asset('image/5.jpeg') }}" />
+            <img class="w-full shadow-lg cursor-pointer rounded-2xl " src="{{ asset('image/2.jpeg') }}" />
+            <img class="w-full shadow-lg cursor-pointer rounded-2xl " src="{{ asset('image/3.jpg') }}" />
+            <img class="w-full shadow-lg cursor-pointer rounded-2xl " src="{{ asset('image/4.jpeg') }}" />
+            <img class="w-full shadow-lg cursor-pointer rounded-2xl " src="{{ asset('image/1.jpeg') }}" />
+            <img class="w-full shadow-lg cursor-pointer rounded-2xl " src="{{ asset('image/5.jpeg') }}" />
+            <img class="w-full shadow-lg cursor-pointer rounded-2xl " src="{{ asset('image/1.jpeg') }}" />
+            <img class="w-full shadow-lg cursor-pointer rounded-2xl " src="{{ asset('image/2.jpeg') }}" />
+            <img class="w-full shadow-lg cursor-pointer rounded-2xl " src="{{ asset('image/3.jpg') }}" />
+            <img class="w-full shadow-lg cursor-pointer rounded-2xl " src="{{ asset('image/4.jpeg') }}" />
+            <img class="w-full shadow-lg cursor-pointer rounded-2xl " src="{{ asset('image/5.jpeg') }}" />
 
-        <img class="w-full " src="{{ asset('image/2.jpeg') }}" />
-        <img class="w-full " src="{{ asset('image/3.jpg') }}" />
-        <img class="w-full " src="{{ asset('image/4.jpeg') }}" />
-        <img class="w-full " src="{{ asset('image/1.jpeg') }}" />
-        <img class="w-full " src="{{ asset('image/5.jpeg') }}" />
-        <img class="w-full " src="{{ asset('image/1.jpeg') }}" />
-        <img class="w-full " src="{{ asset('image/2.jpeg') }}" />
-        <img class="w-full " src="{{ asset('image/3.jpg') }}" />
-        <img class="w-full " src="{{ asset('image/4.jpeg') }}" />
-        <img class="w-full " src="{{ asset('image/5.jpeg') }}" />
+            <img class="w-full shadow-lg cursor-pointer rounded-2xl " src="{{ asset('image/2.jpeg') }}" />
+            <img class="w-full shadow-lg cursor-pointer rounded-2xl " src="{{ asset('image/3.jpg') }}" />
+            <img class="w-full shadow-lg cursor-pointer rounded-2xl " src="{{ asset('image/4.jpeg') }}" />
+            <img class="w-full shadow-lg cursor-pointer rounded-2xl " src="{{ asset('image/1.jpeg') }}" />
+            <img class="w-full shadow-lg cursor-pointer rounded-2xl " src="{{ asset('image/5.jpeg') }}" />
+            <img class="w-full shadow-lg cursor-pointer rounded-2xl " src="{{ asset('image/1.jpeg') }}" />
+            <img class="w-full shadow-lg cursor-pointer rounded-2xl " src="{{ asset('image/2.jpeg') }}" />
+            <img class="w-full shadow-lg cursor-pointer rounded-2xl " src="{{ asset('image/3.jpg') }}" />
+            <img class="w-full shadow-lg cursor-pointer rounded-2xl " src="{{ asset('image/4.jpeg') }}" />
+            <img class="w-full shadow-lg cursor-pointer rounded-2xl " src="{{ asset('image/5.jpeg') }}" />
 
-        <img class="w-full " src="{{ asset('image/2.jpeg') }}" />
-        <img class="w-full " src="{{ asset('image/3.jpg') }}" />
-        <img class="w-full " src="{{ asset('image/4.jpeg') }}" />
-        <img class="w-full " src="{{ asset('image/1.jpeg') }}" />
-        <img class="w-full " src="{{ asset('image/5.jpeg') }}" />
-        <img class="w-full " src="{{ asset('image/1.jpeg') }}" />
-        <img class="w-full " src="{{ asset('image/2.jpeg') }}" />
-        <img class="w-full " src="{{ asset('image/3.jpg') }}" />
-        <img class="w-full " src="{{ asset('image/4.jpeg') }}" />
-        <img class="w-full " src="{{ asset('image/5.jpeg') }}" />
-
-        <img class="w-full " src="{{ asset('image/2.jpeg') }}" />
-        <img class="w-full " src="{{ asset('image/3.jpg') }}" />
-        <img class="w-full " src="{{ asset('image/4.jpeg') }}" />
-        <img class="w-full " src="{{ asset('image/1.jpeg') }}" />
-        <img class="w-full " src="{{ asset('image/5.jpeg') }}" />
-        <img class="w-full " src="{{ asset('image/5.jpeg') }}" />
-        <img class="w-full " src="{{ asset('image/5.jpeg') }}" />
-        <img class="w-full " src="{{ asset('image/5.jpeg') }}" />
-        <img class="w-full " src="{{ asset('image/5.jpeg') }}" />
+            <img class="w-full shadow-lg cursor-pointer rounded-2xl " src="{{ asset('image/2.jpeg') }}" />
+            <img class="w-full shadow-lg cursor-pointer rounded-2xl " src="{{ asset('image/3.jpg') }}" />
+            <img class="w-full shadow-lg cursor-pointer rounded-2xl " src="{{ asset('image/4.jpeg') }}" />
+            <img class="w-full shadow-lg cursor-pointer rounded-2xl " src="{{ asset('image/1.jpeg') }}" />
+            <img class="w-full shadow-lg cursor-pointer rounded-2xl " src="{{ asset('image/5.jpeg') }}" />
+            <img class="w-full shadow-lg cursor-pointer rounded-2xl " src="{{ asset('image/5.jpeg') }}" />
+            <img class="w-full shadow-lg cursor-pointer rounded-2xl " src="{{ asset('image/5.jpeg') }}" />
+            <img class="w-full shadow-lg cursor-pointer rounded-2xl " src="{{ asset('image/5.jpeg') }}" />
+            <img class="w-full shadow-lg cursor-pointer rounded-2xl " src="{{ asset('image/5.jpeg') }}" />
+        </div>
 
     </div>
 </body>

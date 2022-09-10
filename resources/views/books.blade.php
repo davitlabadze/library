336216e103a55543448e61c9bab6521e341919b1
davitlabadze <labadzedato18@gmail.com>
2022-09-10 17:39:50 +0400

    style: wip books page

--- resources/views/books.blade.php
+++ resources/views/books.blade.php
@@ -5,157 +5,51 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>BookShelf</title>
     @vite('resources/css/app.css')
 
 </head>
 
 <body>
+    <div class="container gap-8 p-4 mx-auto space-y-3 columns-6">
 
-    <div class="container mx-auto text-center ">
+        <img class="w-full " src="{{ asset('image/1.jpeg') }}" />
+        <img class="w-full " src="{{ asset('image/2.jpeg') }}" />
+        <img class="w-full " src="{{ asset('image/3.jpg') }}" />
+        <img class="w-full " src="{{ asset('image/4.jpeg') }}" />
+        <img class="w-full " src="{{ asset('image/5.jpeg') }}" />
 
-        <div class="">
-            <table class="absolute top-0  border-blue-800 bg-[url('../../public/image/shelf.png')]  bg-contain ">
-                <tbody class="grid grid-cols-3 ml-6">
-                    {{-- section1 --}}
-                    <tr class='ml-2 py-11 hover:bg-red-500'>
-                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    </tr>
-                    <tr class='py-11 hover:bg-red-500'>
-                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    </tr>
-                    <tr class='py-11 hover:bg-red-500'>
-                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    </tr>
-                    <tr class='py-11 hover:bg-red-500'>
-                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    </tr>
-                    <tr class='py-11 hover:bg-red-500'>
-                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    </tr>
-                    <tr class='py-11 hover:bg-red-500'>
-                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    </tr>
-                    <tr class='py-11 hover:bg-red-500'>
-                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    </tr>
-                    <tr class='py-11 hover:bg-red-500'>
-                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    </tr>
-                    <tr class='py-11 hover:bg-red-500'>
-                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    </tr>
-                    <tr class='py-11 hover:bg-red-500'>
-                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    </tr>
-                    <tr class='py-11 hover:bg-red-500'>
-                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    </tr>
-                    <tr class='py-11 hover:bg-red-500'>
-                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    </tr>
-                    {{-- section 2 --}}
-                    <tr class='ml-2 py-11 hover:bg-red-500'>
-                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    </tr>
-                    <tr class='py-11 hover:bg-red-500'>
-                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    </tr>
-                    <tr class='py-11 hover:bg-red-500'>
-                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    </tr>
-                    <tr class='py-11 hover:bg-red-500'>
-                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    </tr>
-                    <tr class='py-11 hover:bg-red-500'>
-                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    </tr>
-                    <tr class='py-11 hover:bg-red-500'>
-                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    </tr>
-                    <tr class='py-11 hover:bg-red-500'>
-                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    </tr>
-                    <tr class='py-11 hover:bg-red-500'>
-                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    </tr>
-                    <tr class='py-11 hover:bg-red-500'>
-                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    </tr>
-                    <tr class='py-11 hover:bg-red-500'>
-                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    </tr>
-                    <tr class='py-11 hover:bg-red-500'>
-                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    </tr>
-                    <tr class='py-11 hover:bg-red-500'>
-                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    </tr>
-                    {{-- section 3 --}}
-                    <tr class='ml-2 py-11 hover:bg-red-500'>
-                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    </tr>
-                    <tr class='py-11 hover:bg-red-500'>
-                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    </tr>
-                    <tr class='py-11 hover:bg-red-500'>
-                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    </tr>
-                    <tr class='py-11 hover:bg-red-500'>
-                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    </tr>
-                    <tr class='py-11 hover:bg-red-500'>
-                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    </tr>
-                    <tr class='py-11 hover:bg-red-500'>
-                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    </tr>
-                    <tr class='py-11 hover:bg-red-500'>
-                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    </tr>
-                    <tr class='py-11 hover:bg-red-500'>
-                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    </tr>
-                    <tr class='py-11 hover:bg-red-500'>
-                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    </tr>
-                    <tr class='py-11 hover:bg-red-500'>
-                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    </tr>
-                    <tr class='py-11 hover:bg-red-500'>
-                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    </tr>
-                    <tr class='py-11 hover:bg-red-500'>
-                        <td class="p-20 ">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    </tr>
-                </tbody>
+        <img class="w-full " src="{{ asset('image/2.jpeg') }}" />
+        <img class="w-full " src="{{ asset('image/3.jpg') }}" />
+        <img class="w-full " src="{{ asset('image/4.jpeg') }}" />
+        <img class="w-full " src="{{ asset('image/1.jpeg') }}" />
+        <img class="w-full " src="{{ asset('image/5.jpeg') }}" />
+        <img class="w-full " src="{{ asset('image/1.jpeg') }}" />
+        <img class="w-full " src="{{ asset('image/2.jpeg') }}" />
+        <img class="w-full " src="{{ asset('image/3.jpg') }}" />
+        <img class="w-full " src="{{ asset('image/4.jpeg') }}" />
+        <img class="w-full " src="{{ asset('image/5.jpeg') }}" />
 
-            </table>
-            {{-- <div class="= grid grid-cols-3 -mt-5 ">
-                <div class="p-12 bg-red-500">
-                    test
-                </div>
-                <div class="bg-blue-500">
-                    <h1>test</h1>
+        <img class="w-full " src="{{ asset('image/2.jpeg') }}" />
+        <img class="w-full " src="{{ asset('image/3.jpg') }}" />
+        <img class="w-full " src="{{ asset('image/4.jpeg') }}" />
+        <img class="w-full " src="{{ asset('image/1.jpeg') }}" />
+        <img class="w-full " src="{{ asset('image/5.jpeg') }}" />
+        <img class="w-full " src="{{ asset('image/1.jpeg') }}" />
+        <img class="w-full " src="{{ asset('image/2.jpeg') }}" />
+        <img class="w-full " src="{{ asset('image/3.jpg') }}" />
+        <img class="w-full " src="{{ asset('image/4.jpeg') }}" />
+        <img class="w-full " src="{{ asset('image/5.jpeg') }}" />
 
-                </div>
-                <div class="bg-green-500">
-                    <h1>test</h1>
-
-                </div>
-                <div class="mt-12 bg-red-500 ">
-                    test
-                </div>
-                <div class="bg-blue-500">
-                    <h1>test</h1>
-
-                </div>
-                <div class="bg-green-500">
-                    <h1>test</h1>
-
-                </div>
-            </div> --}}
-        </div>
+        <img class="w-full " src="{{ asset('image/2.jpeg') }}" />
+        <img class="w-full " src="{{ asset('image/3.jpg') }}" />
+        <img class="w-full " src="{{ asset('image/4.jpeg') }}" />
+        <img class="w-full " src="{{ asset('image/1.jpeg') }}" />
+        <img class="w-full " src="{{ asset('image/5.jpeg') }}" />
+        <img class="w-full " src="{{ asset('image/5.jpeg') }}" />
+        <img class="w-full " src="{{ asset('image/5.jpeg') }}" />
+        <img class="w-full " src="{{ asset('image/5.jpeg') }}" />
+        <img class="w-full " src="{{ asset('image/5.jpeg') }}" />
 
     </div>
 </body>

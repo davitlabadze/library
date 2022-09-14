e836cbf1a5dd68abe4e04a0310aa7383e37866e4
davitlabadze <labadzedato18@gmail.com>
2022-09-15 00:22:39 +0400

    style: change adminpanel color

--- resources/views/layouts/adminPanelLayout.blade.php
+++ resources/views/layouts/adminPanelLayout.blade.php
@@ -1,76 +1,93 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.10/css/select2.min.css" rel="stylesheet"/>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.10/css/select2.min.css" rel="stylesheet" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.10/js/select2.min.js"></script>
     <title>Dashboard</title>
 
 </head>
+
 <body>
     <div>
         <div class="hidden md:flex md:w-64 md:flex-col md:fixed md:inset-y-0">
-        <div class="flex flex-col flex-1 min-h-0 bg-gray-800">
-            <div class="flex items-center flex-shrink-0 h-16 px-4 bg-gray-900">
-                <h1 class="text-xl font-bold text-white">Adminpanel</h1>
+            <div class="flex flex-col flex-1 min-h-0 bg-orange-400">
+                <div class="flex items-center flex-shrink-0 h-16 px-4 bg-orange-500">
+                    <h1 class="text-xl font-bold text-white">Adminpanel</h1>
+                </div>
+                <div class="flex flex-col flex-1 overflow-y-auto">
+                    <nav class="flex-1 px-2 py-4 space-y-1">
+                        <div class="flex items-center px-2 py-2 text-sm font-medium text-white">
+                            CORE
+                        </div>
+                        <a href="{{ route('dashboard') }}"
+                            class="flex items-center px-2 py-2 text-sm font-medium text-white bg-orange-500 rounded-md group">
+                            <svg class="flex-shrink-0 w-6 h-6 mr-3 text-white" xmlns="http://www.w3.org/2000/svg"
+                                fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+                            </svg>
+                            Dashboard
+                        </a>
+                        <a href="{{ url('/') }}"
+                            class="flex items-center px-2 py-2 text-sm font-medium text-white rounded-md hover:bg-orange-300 hover:text-white group"
+                            target="_blank">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="flex-shrink-0 w-6 h-6 mr-3 text-white"
+                                fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
+                            </svg>
+                            View Website
+                        </a>
+                        <div class="flex items-center px-2 py-2 text-sm font-medium text-white">
+                            INTERFACE
+                        </div>
+                        <a href="{{ route('authors.index') }}"
+                            class="flex items-center px-2 py-2 text-sm font-medium text-white rounded-md hover:bg-orange-300 hover:text-white group">
+                            <svg xmlns="http://www.w3.org/2000/svg"
+                                class="flex-shrink-0 w-6 h-6 mr-3 text-white group-hover:text-white" fill="none"
+                                viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                            </svg>
+                            Authors
+                        </a>
+                        <a href="{{ route('books.index') }}"
+                            class="flex items-center px-2 py-2 text-sm font-medium text-white rounded-md hover:bg-orange-300 hover:text-white group">
+                            <svg xmlns="http://www.w3.org/2000/svg"
+                                class="flex-shrink-0 w-6 h-6 mr-3 text-white group-hover:text-white" fill="none"
+                                viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
+                            </svg>
+                            Books
+                        </a>
+                    </nav>
+                </div>
             </div>
-            <div class="flex flex-col flex-1 overflow-y-auto">
-                <nav class="flex-1 px-2 py-4 space-y-1">
-                    <div class="flex items-center px-2 py-2 text-sm font-medium text-gray-500">
-                        CORE
+        </div>
+        <div class="flex flex-col md:pl-64">
+            <div class="sticky top-0 z-10 flex flex-shrink-0 h-16 bg-white shadow">
+                <div class="flex justify-between flex-1 px-4">
+                    <div class="flex flex-1 mt-4">Dashboard</div>
+                    <div class="flex items-center ml-4 md:ml-6">
+                        <div class="relative ml-3">
+                        </div>
                     </div>
-                    <a href="{{ route('dashboard') }}" class="flex items-center px-2 py-2 text-sm font-medium text-white bg-gray-900 rounded-md group">
-                        <svg class="flex-shrink-0 w-6 h-6 mr-3 text-gray-300" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-                        </svg>
-                    Dashboard
-                    </a>
-                    <a href="{{ url('/') }}" class="flex items-center px-2 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white group" target="_blank">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="flex-shrink-0 w-6 h-6 mr-3 text-gray-300" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
-                        </svg>
-                    View Website
-                    </a>
-                    <div class="flex items-center px-2 py-2 text-sm font-medium text-gray-500">
-                        INTERFACE
-                    </div>
-                    <a href="{{ route('authors.index') }}" class="flex items-center px-2 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white group">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="flex-shrink-0 w-6 h-6 mr-3 text-gray-400 group-hover:text-gray-300" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-                          </svg>
-                    Authors
-                    </a>
-                    <a href="{{ route('books.index') }}" class="flex items-center px-2 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white group">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="flex-shrink-0 w-6 h-6 mr-3 text-gray-400 group-hover:text-gray-300" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
-                          </svg>
-                    Books
-                    </a>
-                </nav>
+                </div>
             </div>
+            <main class="w-full p-16 mx-auto mt-2 bg-gray-100 border border-gray-200 rounded-xl">
+                <div class="w-full">
+                    @yield('content')
+                </div>
+            </main>
         </div>
     </div>
-    <div class="flex flex-col md:pl-64">
-      <div class="sticky top-0 z-10 flex flex-shrink-0 h-16 bg-white shadow">
-        <div class="flex justify-between flex-1 px-4">
-          <div class="flex flex-1 mt-4">Dashboard</div>
-          <div class="flex items-center ml-4 md:ml-6">
-            <div class="relative ml-3">
-            </div>
-          </div>
-        </div>
-      </div>
-      <main class="w-full p-16 mx-auto mt-2 bg-gray-100 border border-gray-200 rounded-xl">
-        <div class="w-full">
-            @yield('content')
-        </div>
-      </main>
-    </div>
-  </div>
 </body>
-</html>
 
+</html>
